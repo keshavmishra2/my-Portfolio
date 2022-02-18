18162c80eecfac25bf03c0e350a41dc3c4526cbc
KeshavKumar Mishra <87552836+keshavmishra2@users.noreply.github.com>
2022-02-18 11:07:24 +0530

    done1

---
+++ src/Utils/Projects.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    img: "https://user-images.githubusercontent.com/87552836/154544711-3403868d-05e4-49b0-ab5f-452a52e09fac.png",
+    title: "Nodestrom",
+    des: "Nodestrom is an American luxury department store chain which deals with the sale of clothing, footwear, handbags, jewelry, accessories, cosmetics and fragrances.In this project I had work on NavBar HomePage and Login and Woman Product Page.",
+    demo: "",
+    live: "https://relaxed-blackwell-8ba8bf.netlify.app/home.html",
+    gitHub: "https://github.com/keshavmishra2/Nordstrom-Project",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    img: "https://user-images.githubusercontent.com/87552836/154543917-e5fc509b-76d9-4b8e-afbc-b3ea1fa2fa7c.png",
+    title: "Cultfit Backend",
+    des: "A website to learn fitness.Order fitness equipment ,healthy food, Mind Therphy and Care.And I implemented some functions in the Product page , Mind Page and Care Page.",
+    demo: "",
+    live: "https://cult-fit-clone-w13.herokuapp.com/",
+    gitHub: "https://github.com/Lpavan6445/Cult.fit-clone",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Mongoose",
+      "ExpressJS",
+      "NodeJs",
+    ],
+  },
+ 
+];
